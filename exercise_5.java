4f9ba701dfc2707188f0b36af0743fff7bc3059e
Andrewgaabriel <andrew.gabrielgomes@gmail.com>
2021-06-22 13:44:02 -0300

    melhora exercicio 5

--- exercise_5.java
+++ exercise_5.java
@@ -1,11 +1,11 @@
 public class exercise_5 {
     public static void main(String[] args) {
         
-        System.out.println("Java atrapalha muita gente");
+        System.out.println("1 Java atrapalha muita gente");
 
-        for (int i=2; i<102; i++) {
+        for (int i=2; i<101; i++) {
             String result = new String(new char[i]).replace("\0", "atrapalham ");
-            System.out.print("Java " + result);
+            System.out.print(i + " Java " + result);
             System.out.println("muita gente");
         }
     }
